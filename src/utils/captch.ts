332df87875146cd12538b836741a7c2fb5230414
Patel Chirag <chiraghpatel03@gmail.com>
2025-09-23 02:20:44 +0530

    temp

--- src/utils/captch.ts
+++ src/utils/captch.ts
@@ -1,20 +1,3 @@
-export function getCaptchaToken(): Promise<string> {
-  return new Promise((res, rej) => {
-    // @ts-expect-error window is not defined
-    if (!window?.grecaptcha || !window?.grecaptcha.enterprise) return rej(new Error('reCAPTCHA not loaded'));
-
-    // @ts-expect-error window is not defined
-    window.grecaptcha.enterprise.ready(async () => {
-      try {
-        // @ts-expect-error window is not defined
-        res(await window.grecaptcha.enterprise.execute(process.env.NEXT_PUBLIC_CAPTCHA_KEY!, {action: 'contact_us'}));
-      } catch (err) {
-        rej(err);
-      }
-    });
-  });
-}
-
 export async function verifyCaptchaToken(token: string) {
   const url = new URL('https://www.google.com/recaptcha/api/siteverify');
   url.searchParams.append('secret', process.env.CAPTCHA_SECRET!);
@@ -22,3 +5,6 @@ export async function verifyCaptchaToken(token: string) {
   const response = await fetch(url.toString(), {method: 'POST'});
   return response.json();
 }
+
+
+const url = `https://recaptchaenterprise.googleapis.com/v1/projects/fabled-sector-448104-i0/assessments?key=${}`
